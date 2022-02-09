62f0470d547601e8de43bba6f3e38973d149618b
William Kwong <work.williamkwong@gmail.com>
2022-02-09 01:20:03 -0500

    Additional changes

--- misc/getFolders.ts
+++ misc/getFolders.ts
@@ -2,6 +2,6 @@ import { Post } from '../lib/types/post';
 
 export default function getFolders(posts: Post[]) {
   return Array.from(
-    new Set(posts.map((post) => post.path.split("\\").filter((p) => !!p)[0]))
+    new Set(posts.map((post) => post.path.split("/").filter((p) => !!p)[0]))
   );
 }
