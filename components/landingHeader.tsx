d90690faa3016a44726290da5033e1038a424dac
William Kwong <work.williamkwong@gmail.com>
2022-03-25 12:26:16 -0400

    Added video and whatnot

--- components/landingHeader.tsx
+++ components/landingHeader.tsx
@@ -5,13 +5,20 @@ import NavMenu from './navMenu';
 
 export default function LandingHeader() {
   return (
-    <header className="bg-primary-light">
-      <div className="flex items-start justify-end max-w-6xl p-8 mx-auto">
-        <NavMenu />
-      </div>
-      <div className="grid grid-rows-2 max-w-6xl px-8 pb-8 mx-auto justify-center">
-        <h1 className="flex text-5xl font-medium mb-4 justify-center">{config.header.title}</h1>
-        <p className="flex text-lg text-center max-w-md justify-center">{config.header.description}</p>
+    <header className="h-screen">
+      <div>
+        <video autoPlay muted loop className= "video">
+          <source src="/static/videos/backgroundvideo.mp4" type="video/mp4"/>
+        </video>
+        <div className="flex items-start justify-end max-w-6xl p-8 mx-auto">
+          <NavMenu />
+        </div>
+        <div className="absolute inset-0 flex justify-center items-center z-10">
+          <div className="grid auto-rows-min max-w-6xl px-8 h-screen mx-auto justify-center content-center">
+            <h1 className="flex text-5xl font-medium mb-4 justify-center">AutoWheelie</h1>
+            <p className="flex text-lg text-center max-w-md justify-center pb-10">Making all cities accessible to wheelchair users</p>
+          </div>
+        </div>
       </div>
     </header>
   );
