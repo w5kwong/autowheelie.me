47ce3c008ad144c259f84b6a08a1106cc19531cb
William Kwong <work.williamkwong@gmail.com>
2022-03-25 12:44:16 -0400

    Hotfix

--- components/landingHeader.tsx
+++ components/landingHeader.tsx
@@ -13,7 +13,7 @@ export default function LandingHeader() {
         <div className="flex items-start justify-end max-w-6xl p-8 mx-auto">
           <NavMenu />
         </div>
-        <div className="absolute inset-0 flex justify-center items-center z-10">
+        <div className="absolute inset-0 flex justify-center items-center z-10 pointer-events-none">
           <div className="grid auto-rows-min max-w-6xl px-8 h-screen mx-auto justify-center content-center">
             <h1 className="flex text-5xl font-medium mb-4 justify-center">AutoWheelie</h1>
             <p className="flex text-lg text-center max-w-md justify-center pb-10">Making all cities accessible to wheelchair users</p>
